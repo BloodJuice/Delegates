d8e5bb1d8039cadb4c3dee1777ae357ddbb8e1da
BloodJuice <falcon.with.hat@gmail.com>
2023-09-05 15:08:44 +0300

    first commit

---
+++ Projects/ExchangeMonitor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Projects
+{
+    public class ExchangeMonitor
+    {
+        public delegate void Price(int price);
+
+        public Price PriceRandom { get; set; }
+        
+        public void Start()
+        {
+            while (true)
+            {
+                int bankPrice = (new Random()).Next(100);
+                PriceRandom(bankPrice);
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ Projects/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExchangeMonitor result = new ExchangeMonitor();
+            result.PriceRandom = Print;
+            result.Start();
+        }
+        public static void Print(int price) => Console.WriteLine($"Price now is {price}");
+    }
+}
